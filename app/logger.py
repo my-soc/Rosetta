f7e31cff6738aecd0fbbe645c5e0a7d093a1ef53
amahmoud <ayman.s.mahmoud@outlook.com>
2023-04-02 11:55:19 +0200

    enable logging

---
+++ app/logger.py
@@ -0,0 +1,24 @@
+import logging
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request
+from starlette.responses import Response
+
+
+class RequestLoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next) -> Response:
+        request_logger = logging.getLogger("request_audit")
+        request_logger.info(
+            f"{request.method} {request.url.path} {request.client.host}"
+        )
+
+        return await call_next(request)
+
+
+audit_log_handler = logging.FileHandler("request_audit.log")
+audit_log_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+audit_log_handler.setLevel(logging.INFO)
+
+request_audit_logger = logging.getLogger("request_audit")
+request_audit_logger.addHandler(audit_log_handler)
+request_audit_logger.setLevel(logging.INFO)
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 from .api.schema import schema
+from .logger import RequestLoggingMiddleware
+
 
 app = FastAPI()
+app.add_middleware(RequestLoggingMiddleware)
 app.add_route("/", GraphQL(schema=schema))
 
 if __name__ == "__main__":
